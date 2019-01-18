b8ca98067401d8eeef110599a18976fcf30c366f
CappuccinoZ <43778933+CappuccinoZ@users.noreply.github.com>
2019-01-19 00:37:31 +0800

    Add files via upload

--- equation/equation.h
+++ equation/equation.h
@@ -5,8 +5,9 @@
 #include <stdlib.h>
 #include <math.h>
 #include <conio.h>
+#include "toolbox.h"
 
-/*éŸ¦è¾¾å®šç†è§£æ–¹ç¨‹ x^2 - px + q = 0*/
+/*Î¤´ï¶¨Àí½â·½³Ì x^2 - px + q = 0*/
 int vieta(double p, double q, int no1, int no2)//p = x1 + x2; q = x1 * x2
 {
 	double delta = p * p - 4 * q;
@@ -27,37 +28,271 @@ int vieta(double p, double q, int no1, int no2)//p = x1 + x2; q = x1 * x2
 	return 0;
 }
 
+/*Êä³öÒ»ÔªÈı´Î·½³ÌµÄÒ»¸öÊµÊı¸ù*/
 double equation3(double a, double b, double c, double d)
 {
 	double p, q, x, delta;
 
-	/*è½¬åŒ–x^3 + b'x^2 + c'x + d' = 0*/
+	/*×ª»¯x^3 + b'x^2 + c'x + d' = 0*/
 	b /= a;
 	c /= a;
 	d /= a;
 
-	/*è½¬åŒ–x^3 + px + q = 0*/
+	/*×ª»¯x^3 + px + q = 0*/
 	p = c - b * b / 3;
 	q = d + b * (2 * b * b - 9 * c) / 27;
 
-	/*åˆ¤åˆ«å¼ (p/3)^3 + (q/2)^2*/
+	/*ÅĞ±ğÊ½ (p/3)^3 + (q/2)^2*/
 	p = -p * p * p / 27;
 	q /= -2;
 	delta = -p + q * q;
 
-	if (delta < 0)//ä¸‰ä¸ªä¸ç­‰å®æ ¹
+	if (delta < 0)//Èı¸ö²»µÈÊµ¸ù
 	{
 		double r = sqrt(p);
 		double theta = acos(q / r) / 3;
 		x = 2 * cbrt(r) * cos(theta) - b / 3;
 	}
-	else//å¡å°”ä¸¹å…¬å¼
+	else//¿¨¶ûµ¤¹«Ê½
 	{
 		double part1 = cbrt(q + sqrt(delta));
 		double part2 = cbrt(q - sqrt(delta));
 		x = part1 + part2 - b / 3;
 	}
 	return x;
+}
+
+/*Ò»ÔªÒ»´Î·½³Ì*/
+double equation1(double a, double b)
+{
+	if (a != 0)
+	{
+		printf("x = %lf.\n", -b / a);
+	}
+	else if(b == 0)
+	{
+		printf("ËùÓĞ¸´Êı.\n");
+	}
+	else
+	{
+		printf("ÎŞ½â.\n");
+	}
+	return 0;
+}
+
+/*Ò»Ôª¶ş´Î·½³Ì*/
+int equation2(void)//Ò»Ôª¶ş´Î·½³Ì
+{
+	double a, b, c;
+	do
+	{
+		printf("ax^2 + bx + c = 0\n");
+		printf("ÇëÊäÈëa:\n");
+		scanf("%lf", &a);
+		checkch();
+		printf("ÇëÊäÈëb:\n");
+		scanf("%lf", &b);
+		checkch();
+		printf("ÇëÊäÈëc:\n");
+		scanf("%lf", &c);
+		checkch();
+		asterisks();
+
+		if (a != 0)
+		{
+			vieta(-b / a, c / a, 1, 2);
+
+			double x = -b / (2 * a);
+			double y = c - b * b / (4 * a);
+			if (a > 0)
+			{
+				printf("Min:(%lf, %lf).\n", x, y);
+			}
+			else
+			{
+				printf("Max:(%lf, %lf).\n", x, y);
+			}
+		}
+		else
+		{
+			equation1(b, c);
+		}
+		asterisks();
+	} while (quit());
+
+	return 0;
+}
+
+/*Ò»Ôª¶ş´Î²»µÈÊ½(¿ªÇø¼ä)*/
+int open2(int k)//Ò»Ôª¶ş´Î²»µÈÊ½(¿ªÇø¼ä)
+{
+	double a, b, c, x1, x2;
+
+	printf("ÇëÊäÈë¶ş´ÎÏîÏµÊı:\n");
+	scanf("%lf", &a);
+	checkch();
+	printf("ÇëÊäÈëÒ»´ÎÏîÏµÊı:\n");
+	scanf("%lf", &b);
+	checkch();
+	printf("ÇëÊäÈë³£ÊıÏî:\n");
+	scanf("%lf", &c);
+	checkch();
+	asterisks();
+	printf("½â¼¯Îª ");
+	if (k)
+	{
+		a *= -1;
+		b *= -1;
+		c *= -1;
+	}
+
+	if (a == 0)
+	{
+		if (b > 0)
+		{
+			x1 = -c / b;
+			printf("(%lf, +¡Ş).\n", x1);
+		}
+		else if (b < 0)
+		{
+			x1 = -c / b;
+			printf("(-¡Ş, %lf).\n", x1);
+		}
+		else if (c > 0)
+		{
+			printf("(-¡Ş, +¡Ş).\n");
+		}
+		else
+		{
+			printf("¿Õ¼¯.\n");
+		}
+	}
+
+	else
+	{
+		double delta = b * b - 4 * a * c;
+		if (a > 0)
+		{
+			if (delta == 0)
+			{
+				x1 = -b / (2 * a);
+				printf("(-¡Ş, %lf)¡È(%lf, +¡Ş).\n", x1, x1);
+			}
+
+			else if (delta > 0)
+			{
+				x1 = (-b - sqrt(delta)) / (2 * a);
+				x2 = (-b + sqrt(delta)) / (2 * a);
+				printf("(-¡Ş, %lf)¡È(%lf, +¡Ş).\n", x1, x2);
+			}
+
+			else
+			{
+				printf("(-¡Ş, +¡Ş).\n");
+			}
+		}
+
+		else
+		{
+			if (delta > 0)
+			{
+				x1 = (-b - sqrt(delta)) / (2 * a);
+				x2 = (-b + sqrt(delta)) / (2 * a);
+				printf("(%lf, %lf).\n", x1, x2);
+			}
+
+			else
+			{
+				printf("¿Õ¼¯.");
+
+			}
+		}
+	}
+	return 0;
+}
+
+/*Ò»Ôª¶ş´Î²»µÈÊ½(±ÕÇø¼ä)*/
+int closed2(int k)//Ò»Ôª¶ş´Î²»µÈÊ½(±ÕÇø¼ä)
+{
+	double a, b, c, x1, x2;
+
+	printf("ÇëÊäÈë¶ş´ÎÏîÏµÊı:\n");
+	scanf("%lf", &a);
+	checkch();
+	printf("ÇëÊäÈëÒ»´ÎÏîÏµÊı:\n");
+	scanf("%lf", &b);
+	checkch();
+	printf("ÇëÊäÈë³£ÊıÏî:\n");
+	scanf("%lf", &c);
+	checkch();
+	asterisks();
+	printf("½â¼¯Îª ");
+
+	if (k)
+	{
+		a *= -1;
+		b *= -1;
+		c *= -1;
+	}
+
+	if (a == 0)
+	{
+		if (b > 0)
+		{
+			x1 = -c / b;
+			printf("[%lf, +¡Ş).\n", x1);
+		}
+		else if (b < 0)
+		{
+			x1 = -c / b;
+			printf("(-¡Ş, %lf]).\n", x1);
+		}
+		else if (c >= 0)
+		{
+			printf("(-¡Ş, +¡Ş).\n");
+		}
+		else
+		{
+			printf("¿Õ¼¯.\n");
+		}
+	}
+
+	else
+	{
+		double delta = b * b - 4 * a * c;
+		if (a > 0)
+		{
+			if (delta > 0)
+			{
+				x1 = (-b + sqrt(delta)) / (2 * a);
+				x2 = (-b - sqrt(delta)) / (2 * a);
+				printf("(-¡Ş, %lf]¡È[%lf, +¡Ş).\n", x2, x1);
+			}
+			else
+			{
+				printf("(-¡Ş, +¡Ş).\n");
+			}
+		}
+		else
+		{
+			if (delta > 0)
+			{
+				x1 = (-b + sqrt(delta)) / (2 * a);
+				x2 = (-b - sqrt(delta)) / (2 * a);
+				printf("[%lf, %lf].\n", x2, x1);
+			}
+			else if (delta == 0)
+			{
+				x1 = -b / (2 * a);
+				printf("{ %lf }.\n", x1);
+			}
+			else
+			{
+				printf("¿Õ¼¯.\n");
+			}
+		}
+	}
+	return 0;
 }
 
 #endif
