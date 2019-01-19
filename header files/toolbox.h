2dbc46659896116a90e1bbd4a5533089f249ae4b
CappuccinoZ <43778933+CappuccinoZ@users.noreply.github.com>
2019-01-19 19:57:58 +0800

    Add files via upload

---
+++ header files/toolbox.h
@@ -0,0 +1,39 @@
+#ifndef _TOOLBOX_H
+#define _TOOLBOX_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+/*void checkch(void);
+int quit(void);
+void asterisks(void);*/
+
+void checkch(void)//处理非法字符
+{
+	char ch;
+	while ((ch = getchar()) != EOF && ch != '\n');
+}
+
+int quit(void)//退出
+{
+	printf("[q:退出]\n");
+	char ch = _getch();
+
+	if (ch != 'q')
+	{
+		system("cls");
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+void asterisks(void)
+{
+	puts("*******************************");
+}
+
+#endif
