bc1968ba5a37d3a89d1cbb2805ccca80a25bae34
CappuccinoZ <43778933+CappuccinoZ@users.noreply.github.com>
2019-01-19 19:58:17 +0800

    Add files via upload

---
+++ header files/coffee.h
@@ -0,0 +1,17 @@
+#ifndef _COFFEE_H
+#define _COFFEE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <math.h>
+#include <conio.h>
+
+int coffee_equation2(void);
+int coffee_equation3(void);
+int inequality(void);
+int numeration(void);
+int prime(void);
+int triple(void);
+
+#endif
