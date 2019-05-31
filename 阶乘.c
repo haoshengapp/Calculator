5ffaa3cc0011c046e6a906bf11089cc3abeeee29
CappuccinoZ <43778933+CappuccinoZ@users.noreply.github.com>
2019-06-01 00:14:40 +0800

    Add files via upload

--- 阶乘.c
+++ 阶乘.c
@@ -2,33 +2,47 @@
 #include <stdlib.h>
 #include <math.h>
 #include <conio.h>
-int size;//位数
-int calc(int n, int* a);//计算
+void calc(int n, int* a);
+int z = 0;//结尾零的个数
 
 int main()
 {
-	int* a;
 	int n, i;
 	puts("[请输入n]");
 	scanf_s("%d", &n);
-	if (n > 1 && n < 1000000)
+	if (n > 1 && n < 500000)
 	{
-		size = (int)((n + 0.5) * log10(n) - 0.434294481903251827651 * n + 1.3990899341790575247825);//斯特林公式
-		a = (int*)calloc(size, 4);
-		*a = 1;
+		i = (int)((n + 0.5) * log10(n) - 0.4342945 * n + 1.3990899);//斯特林公式
+		printf("[%d的阶乘](%d位)\n加载中. . .\n", n, i);
+		int* a = (int*)calloc(i, 4);
 
-		printf("[%d的阶乘](%d位)\n加载中...\n", n, size);
-		calc(n, a);
-		i = size - 1;
-		while (i != -1)
+		if (a)
 		{
-			printf("%d", *(a + i));
-			i--;
+			*a = 1;
+			calc(n, a);
+			if (z < 5)
+			{
+				while (i > 0)
+				{
+					i--;
+					printf("%d", *(a + i));
+				}
+			}
+			else
+			{
+				while (i > z)
+				{
+					i--;
+					printf("%d", *(a + i));
+				}
+				printf("(%d个0)", z);
+			}
+			free(a);
 		}
 		puts("");
 	}
 	else
-	{	
+	{
 		switch (n)
 		{
 		case 0:puts("0!=1"); break;
@@ -36,30 +50,32 @@ int main()
 		default:puts("o(≧口≦)o"); break;
 		}
 	}
-	puts("请按任意键结束");
+	puts("请按任意键结束. . .");
 	_getch();
 	return 0;
 }
 
-int calc(int n, int* a)
+void calc(int n, int* a)
 {
-	int i, j;
-	while (n > 1)
+	int i, j, k, size;
+	for (j = 2; j <= n; j++)
 	{
-		for (i = 0; i < size; i++)
+		size = (int)((j + 0.5) * log10(j) - 0.4342945 * j + 1.3990899);
+		for (i = z; i < size; i++)
 		{
-			if (*(a + i) != 0)
-				* (a + i) *= n;
+			*(a + i) *= j;
 		}
-		for (j = 0; j < size; j++)
+		for (i = z; i < size; i++)
 		{
-			if (*(a + j) > 9)
+			if (*(a + i) > 9)
 			{
-				*(a + (j + 1)) += *(a + j) / 10;
-				*(a + j) %= 10;
+				*(a + (i + 1)) += *(a + i) / 10;
+				*(a + i) %= 10;
 			}
 		}
-		n--;
+		for (z = 0, k = 5; k < j; k *= 5)
+		{
+			z += j / k;
+		}
 	}
-	return 0;
 }
